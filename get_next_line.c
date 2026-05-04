b4d43085fcda7e01a415aab37d7ca2dcbe5acb88
Veya Furst <sfurst@42vienna.com>
2026-05-04 18:50:30 +0200

    work i guess

---
+++ get_next_line.c
@@ -0,0 +1,103 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   get_next_line.c                                   :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: sfurst <sfurst@student.42vienna.com>      #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/05/04 17:24:21 by sfurst           #+#    #+#              */
+/*   Updated: 2026/05/04 18:31:23 by sfurst          ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+int	has_newline(t_gnl *dat)
+{
+	ssize_t	i;
+
+	i = 0;
+	while (i < dat->len)
+	{
+		if (dat->data[i] == '\n')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+int	read_until_line(int fd, t_gnl *dat)
+{
+	char	*buf;
+	ssize_t	bytes;
+
+	buf = malloc(BUFFER_SIZE);
+	if (!buf)
+		return (0);
+	while (!has_newline(dat))
+	{
+		bytes = read(fd, buf, BUFFER_SIZE);
+		if (bytes < 0)
+			return (free(buf), 0);
+		if (bytes == 0)
+			break ;
+		if (!append_data(dat, buf, bytes))
+			return (free(buf), 0);
+	}
+	return (free(buf), 1);
+}
+
+char	*extract_line(t_gnl *dat)
+{
+	char	*line;
+	ssize_t	i;
+	ssize_t	line_len;
+
+	i = 0;
+	while (i < dat->len && dat->data[i] != '\n')
+		i++;
+	if (i < dat->len && dat->data[i] == '\n')
+		i++;
+	line_len = i;
+	line = malloc(line_len + 1);
+	if (!line)
+		return (NULL);
+	return (ft_memcpy(line, dat->data, line_len), line[line_len] = '\0', line);
+}
+
+void	consume_line(t_gnl *dat)
+{
+	ssize_t	i;
+	ssize_t	remaining;
+
+	i = 0;
+	while (i < dat->len && dat->data[i] != '\n')
+		i++;
+	if (i < dat->len && dat->data[i] == '\n')
+		i++;
+	remaining = dat->len - i;
+	if (remaining > 0)
+		ft_memmove(dat->data, dat->data + i, remaining);
+	dat->len = remaining;
+	if (dat->data)
+		dat->data[dat->len] = '\0';
+	if (dat->len == 0)
+		free_stuff(dat);
+}
+
+char	*get_next_line(int fd)
+{
+	static t_gnl	dat;
+	char			*line;
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (NULL);
+	if (!read_until_line(fd, &dat))
+		return (free_stuff(&dat), NULL);
+	if (dat.len == 0)
+		return (free_stuff(&dat), NULL);
+	line = extract_line(&dat);
+	if (!line)
+		return (free_stuff(&dat), NULL);
+	return (consume_line(&dat), line);
+}
---
+++ get_next_line.h
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   get_next_line.h                                   :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: sfurst <sfurst@student.42vienna.com>      #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/05/04 17:24:25 by sfurst           #+#    #+#              */
+/*   Updated: 2026/05/04 18:29:11 by sfurst          ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 1
+# endif
+
+typedef struct s_gnl
+{
+	char	*data;
+	ssize_t	cap;
+	ssize_t	len;
+}			t_gnl;
+
+int			append_data(t_gnl *dat, char *buf, ssize_t bytes);
+void		free_stuff(t_gnl *dat);
+void		*ft_memcpy(void *dst, const void *src, size_t n);
+void		*ft_memmove(void *dst, const void *src, size_t len);
+size_t		ft_strlen(const char *s);
+char		*get_next_line(int fd);
+
+#endif
