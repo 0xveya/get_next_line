67f63e961a9480063225163dcb60d16dd99f3db5
Veya Furst <sfurst@42vienna.com>
2026-05-04 18:50:43 +0200

    bonus

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -6,12 +6,12 @@
 /*   By: sfurst <sfurst@student.42vienna.com>      #+#  +:+       +#+         */
 /*                                               +#+#+#+#+#+   +#+            */
 /*   Created: 2026/05/04 17:24:25 by sfurst           #+#    #+#              */
-/*   Updated: 2026/05/04 19:01:40 by sfurst          ###   ########.fr        */
+/*   Updated: 2026/05/04 19:00:31 by sfurst          ###   ########.fr        */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <limits.h>
 # include <stdlib.h>
@@ -19,6 +19,10 @@
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 1
+# endif
+
+# ifndef MAX_FD
+#  define MAX_FD 1024
 # endif
 
 typedef struct s_gnl
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                       :::      ::::::::    */
+/*   get_next_line_utils_bonus.c                       :+:      :+:    :+:    */
+/*                                                   +:+ +:+         +:+      */
+/*   By: sfurst <sfurst@student.42vienna.com>      #+#  +:+       +#+         */
+/*                                               +#+#+#+#+#+   +#+            */
+/*   Created: 2026/05/04 17:24:16 by sfurst           #+#    #+#              */
+/*   Updated: 2026/05/04 19:01:25 by sfurst          ###   ########.fr        */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*dest_char;
+	unsigned char	*src_char;
+
+	dest_char = (unsigned char *)dst;
+	src_char = (unsigned char *)src;
+	if (!dst && !src)
+		return (dst);
+	if (src < dst)
+		while (len--)
+			dest_char[len] = src_char[len];
+	else
+		while (len--)
+			*dest_char++ = *src_char++;
+	return (dst);
+}
+
+void	*ft_memcpy(void *dst, const void *src, size_t n)
+{
+	unsigned char	*dstc;
+	unsigned char	*srcc;
+
+	dstc = (unsigned char *)dst;
+	srcc = (unsigned char *)src;
+	while (n--)
+		*dstc++ = *srcc++;
+	return (dst);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while ((s[i]) && (i++, 1))
+		;
+	return (i);
+}
+
+void	free_stuff(t_gnl *dat)
+{
+	if (dat->data)
+		free(dat->data);
+	dat->data = NULL;
+	dat->cap = 0;
+	dat->len = 0;
+}
+
+int	append_data(t_gnl *dat, char *buf, ssize_t bytes)
+{
+	char	*new;
+	ssize_t	new_cap;
+
+	if (dat->len + bytes + 1 > dat->cap)
+	{
+		new_cap = dat->cap;
+		if (new_cap == 0)
+			new_cap = bytes + 1;
+		while (new_cap < dat->len + bytes + 1)
+			new_cap *= 2;
+		new = malloc(new_cap);
+		if (!new)
+			return (0);
+		if (dat->data)
+			ft_memcpy(new, dat->data, dat->len);
+		free(dat->data);
+		dat->data = new;
+		dat->cap = new_cap;
+	}
+	ft_memcpy(dat->data + dat->len, buf, bytes);
+	return (dat->len += bytes, dat->data[dat->len] = '\0', 1);
+}
